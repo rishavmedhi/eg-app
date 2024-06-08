9dd04fa1d3ebe60e8b0d69d0e960d0e2bb6cddf0
Rishav Medhi <rishav164@gmail.com>
2024-06-08 19:06:57 +0530

    Client side codesetup, login page UI

---
+++ client/src/main.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './index.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
